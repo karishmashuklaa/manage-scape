411e195f48b5095405dc2588e510024475bf1a56
karishmas6 <carishmashukla28@gmail.com>
2021-06-08 19:55:12 +0530

    API endpoints

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,20 +1,23 @@
+import axios from 'axios'
 
 export default async function (req,res) {
 
-   if(req.method == "GET"){
+   if(req.method === "GET"){
       const dataRes = await fetch("http://localhost:3001/api/resources")
       const data = await dataRes.json()
       return res.send(data)
    }
 
-   if(req.method == "POST"){
+   if(req.method === "POST"){
       const {title,description,link,priority,timeToFinish} = req.body
 
       if(!title || !description || !link || !timeToFinish){
 
          return res.status(400).send('Please fill all form fields')
       }
-      return res.send('Your response has been successfully recieved')
+      axios.post('http://localhost:3001/api/resources', req.body)
+
+      return res.send('Your response has been recieved successfully')
 
    }
 
