b9750dcb6394fcd83ed88ded852e9dbcbf4cca59
karishmas6 <carishmashukla28@gmail.com>
2021-06-11 23:50:50 +0530

    Resource edit

--- pages/resources/[id]/edit.js
+++ pages/resources/[id]/edit.js
@@ -1,11 +1,23 @@
 import Layout from "../../../components/Layout"
 
-const EditResource = () => {
+const EditResource = ({resource}) => {
     return (
         <Layout>
-            I am edit page
+            I am edit page of {resource.title} 
         </Layout>
     )
+}
+
+export async function getServerSideProps({params}) {
+
+    const resData = await fetch(`http://localhost:3001/api/resources/${params.id}`)
+    const data = await resData.json()
+
+    return {
+        props: {
+            resource: data
+        }
+    }
 }
 
 export default EditResource
