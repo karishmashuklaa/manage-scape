68f42a441764d33ccaf3fb478db597b57e63e9a8
karishmas6 <carishmashukla28@gmail.com>
2021-06-12 02:15:50 +0530

    Resource edit

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -8,16 +8,19 @@ export default async function (req,res) {
       return res.send(data)
    }
 
-   if(req.method === "POST"){
-      const {title,description,link,priority,timeToFinish} = req.body
+   if(req.method === "POST" || req.method === "PATCH"){
+      const {id,title,description,link,priority,timeToFinish} = req.body
 
       if(!title || !description || !link || !timeToFinish){
 
          return res.status(400).send('Please fill all form fields')
       }
 
+      const url = req.method === "POST" ? ('http://localhost:3001/api/resources') : (`http://localhost:3001/api/resources/${id}`)
+
+
       try {
-         const axiosRes = await axios.post('http://localhost:3001/api/resources', req.body) 
+         const axiosRes = await axios[req.method.toLowerCase()](url, req.body) 
          return res.send(axiosRes.data) 
       } catch (error) {
          return res.status(400).send('Failed to store data')
